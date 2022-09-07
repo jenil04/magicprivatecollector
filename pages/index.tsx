4b042d22db17781df5ca8aec20240d9bc86bcfc3
Daniela Eichner <daniela@nameface.com>
2022-09-06 22:31:34 -0400

    initial website boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+
+export default function Index() {
+  return (
+    <div className="bg-white">
+      <div className="bg-gray-900">
+        <main>
+          <p className="text-white">Hello World!</p>
+        </main>
+
+      </div>
+    </div>
+  )
+}
