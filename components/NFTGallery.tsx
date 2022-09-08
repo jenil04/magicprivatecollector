e2e50c17bb9cafc6988fd57f00f11b2c7d6a06c3
Ned <slide004@gmail.com>
2022-09-08 16:15:10 -0400

    add in routing and template

--- components/NFTGallery.tsx
+++ components/NFTGallery.tsx
@@ -13,11 +13,13 @@ const NFTGallery = (props: any) => {
           className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
         >
           <div className="aspect-w-3 aspect-h-4 bg-gray-200 group-hover:opacity-75 sm:aspect-none sm:h-96">
+            <a href="">
             <img
               src={nft.metadataObj.image}
               alt={nft.metadataObj.name}
               className="h-full w-full object-cover object-center sm:h-full sm:w-full"
             />
+            </a>
           </div>
           <div className="flex flex-1 flex-col space-y-2 p-4">
             <h3 className="text-sm font-medium text-gray-900">
