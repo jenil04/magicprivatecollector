2005901f47f0256e11c026f1848337ca94b5550a
ned <slide004@gmail.com>
2022-09-09 19:26:28 -0400

    more formatting 2

--- pages/nft/[chainId]/[contractAddress]/[tokenId].tsx
+++ pages/nft/[chainId]/[contractAddress]/[tokenId].tsx
@@ -9,8 +9,8 @@ const DetailPage = (props: any) => {
     <div className="bg-white">
 
       <main className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-         
-        <NFTDetailPage nft= {nft} />
+
+        <NFTDetailPage nft={nft} />
       </main>
 
     </div>
@@ -22,26 +22,26 @@ export default DetailPage;
 
 export const getServerSideProps = async (context: any) => {
 
-  const {chainId,contractAddress, tokenId } = context.query;
+  const { chainId, contractAddress, tokenId } = context.query;
 
-  
+
   const url = `https://deep-index.moralis.io/api/v2/nft/${contractAddress}/${tokenId}?chain=${chainId}&format=decimal`;
-   
-  
+
+
   const result = await axios.get(url, {
     headers: {
       'Accept': 'application/json',
-        'X-API-Key': 'h9U7pEVDckfIrOATb5iUnzCuSekKSkpTHqSdrl2ST5WVuN02PI3zA7oVbwtSmPMP'
+      'X-API-Key': 'h9U7pEVDckfIrOATb5iUnzCuSekKSkpTHqSdrl2ST5WVuN02PI3zA7oVbwtSmPMP'
     }
-});
+  });
 
 
-  const nft = {...result.data, metadataObj: JSON.parse(result.data.metadata)};
+  const nft = { ...result.data, metadataObj: JSON.parse(result.data.metadata) };
 
   return {
     props: {
       nft,
     },
-    
+
   }
 }
