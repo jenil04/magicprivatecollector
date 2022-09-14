57ba2a247431abf0773952f367666e176943cb0d
ned <slide004@gmail.com>
2022-09-13 20:55:32 -0400

    initial setup of the detail page wiht questions about what data we wanna show?

--- components/NFTDetailPage.tsx
+++ components/NFTDetailPage.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 const NFTDetailPage = (props: any) => {
   const nft = props.nft;
- 
+  console.log(nft);
   return (
-    <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+    <>
+      <h1 className='text-3xl font-semibold mb-4'>
+        {nft.metadataObj.name}
+      </h1>
+      <div className="rounded-lg border border-gray-200 bg-gray-800 mb-5">
+        <div className="font-medium space-y-2 p-4">
+          <p>
+            <span className="font-light uppercase">Collection:</span> {nft.name}
+          </p>
+          <p>
+            <span className="font-light uppercase">Description:</span> {nft.metadataObj.description}
+          </p>
+          <p>
+            <span className="font-light uppercase">Other metadata:</span> what metadata?
+          </p>
+          <p>
+            <span className="font-light uppercase">Price:</span> do we want price? (what price?)
+          </p>
+        </div>
+      </div>
+      <div className="rounded-lg border border-gray-200">
+        <div className="">
+          <Image
+            src={nft.metadataObj.image}
+            alt={nft.metadataObj.name}
+            className="h-full w-full object-fill object-center sm:h-full sm:w-full rounded-lg"
+            layout='responsive'
+            height={100}
+            width={100}
+          />
+        </div>
 
-      COLLECTION: {nft.name}
-
-    </div>
+      </div>
+    </>
   );
 };
 
--- pages/nft/[chainId]/[contractAddress]/[tokenId].tsx
+++ pages/nft/[chainId]/[contractAddress]/[tokenId].tsx
@@ -6,15 +6,7 @@ const DetailPage = (props: any) => {
   const nft = props.nft;
 
   return (
-    <div className="bg-white">
-
-      <main className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-
-        <NFTDetailPage nft={nft} />
-      </main>
-
-    </div>
-
+    <NFTDetailPage nft={nft} />
   )
 }
 
