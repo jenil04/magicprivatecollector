1fb7d05e1d60ecd3ea25dd0c1f861543050b36a4
Ned <slide004@gmail.com>
2022-09-23 14:20:16 -0400

    draft about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
         About Magic Private Collector
       </h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor velit dolor, non ornare eros posuere non. Pellentesque vitae sodales enim. Donec nec tellus lacinia, dapibus ligula id, tempor dui. Cras dapibus nisi at gravida venenatis. Sed augue ante, accumsan sit amet placerat quis, feugiat et diam. Praesent feugiat lorem dignissim imperdiet lacinia. Aliquam libero tortor, hendrerit nec dapibus quis, sollicitudin id eros.
+        Magic Private Collector (MPC) @TODO is OR will be // your one stop shop for creating, browsing, and buying private NFTs. It is a chain agnostic solution requiring no custom token to function. With an easy to use interface, MPC enables users with just a cryptocurrency wallet to create or buy NFTs which can be kept, shared, or sold. However, each NFT requires a user to provide proof of ownership via their wallet in order to use and view. There are myriad use cases for MPC including any secure content, time or quantity limited NFTs, content one wishes to only privately share or sell, and more. 
       </p>
       <h2 className='text-lg text-mwt font-medium pt-4'>
         About Magic Wizard Tech
